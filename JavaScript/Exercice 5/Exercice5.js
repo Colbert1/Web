d56932436b6209eb67b5c2ab635d975aa7d0a9ba
Colbert1 <gcolbert@la-providence.net>
2020-01-13 16:59:04 +0100

    end ex5

--- JavaScript/Exercice 5/Exercice5.js
+++ JavaScript/Exercice 5/Exercice5.js
@@ -1,10 +1,17 @@
 function boucle() {
     var Tab = [-2, 1, 4];
-    for (i = 0; i < 2; i++)
-    {
-        
+    for (i = 0; i < 3; i++) {
+        resultat = i * i;
+        alert(resultat);
     }
 }
 function boucle2() {
+    var tailleTab = prompt("Quelle taille de tableau désires-tu ? ");
 
+    for (i = 0; i < tailleTab; i++) {
+
+        resultat = i * i;
+        alert(resultat);
+    }
 }
+
