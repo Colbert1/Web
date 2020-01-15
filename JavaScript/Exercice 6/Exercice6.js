b6a4f03c034c441a5df7d0f17b3d2b5242eb0213
Colbert1 <gcolbert@la-providence.net>
2020-01-15 14:10:12 +0100

    ex6

---
+++ JavaScript/Exercice 6/Exercice6.js
@@ -0,0 +1,18 @@
+function boucle(x) {
+    var tab = [-2, 1, 4];
+
+        var resultat = x - 2;
+        if (resultat >= 0) {
+
+            alert(resultat);
+        } else {
+
+            alert("Nombre négatif");
+        }
+    }
+
+    function affiche() {
+
+        soustrait(tab[0]);
+        soustrait(tab[tab.length - 1]);
+    }
