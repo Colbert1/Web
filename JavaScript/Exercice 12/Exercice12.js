2e3d71d3146599352a3a1005b17cbd1aa5d3633d
Colbert1 <gcolbert@la-providence.net>
2020-01-16 10:51:05 +0100

    Resstart Ex10-12

--- JavaScript/Exercice 12/Exercice12.js
+++ JavaScript/Exercice 12/Exercice12.js
@@ -1,19 +1,15 @@
-function initialise()
-		{
-			window.resizeTo(600, 500);
-		}
-		
-	function deplace()
-		{
-			window.moveTo(50, 50);
-		}
-		
-	function deplace_relatif()
-		{
-			window.moveBy(50, 50);
-		}
-		
-	function redimensionne()
-		{
-			window.resizeBy(100, -100);
-		}
+function initialise() {
+	window.resizeTo(600, 500);
+}
+
+function deplace() {
+	window.moveTo(50, 50);
+}
+
+function deplace_relatif() {
+	window.moveBy(50, 50);
+}
+
+function redimensionne() {
+	window.resizeBy(100, -100);
+}
