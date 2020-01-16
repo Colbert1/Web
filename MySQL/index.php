e3678f984c7b4a98d011ff4ad9ca31777c6dad63
Colbert1 <gcolbert@la-providence.net>
2020-01-16 14:48:16 +0100

    reorganisation dossier

---
+++ MySQL/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <link href="style.css" rel="stylesheet">
+    <title>ExPHP</title>
+</head>
+
+<body>
+    <h1>
+        <?php echo "MYSQL"; ?>
+    </h1>
+
+    <body>
+        <div id="divaccueil">
+            <a href="Exercice 1/index.php">Exercice 1</a><br>
+            <a href="Exercice 2/index.php">Exercice 2</a><br>
+            <a href="Exercice 3/index.php">Exercice 3</a><br>
+            <a href="Exercice 4/index.php">Exercice 4</a><br>
+
+            <br><a href="../index.php">Retour</a><br>
+        </div>
+    </body>
+</body>
+
+</html>
